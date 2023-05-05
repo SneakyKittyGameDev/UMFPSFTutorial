7fd83ce5d4a9c8c8f5ea0d0ef2c7fb2be3dde4e6
Sneaky Kitty <codydawe199@gmail.com>
2023-05-05 12:26:20 -0400

    Initial commit

---
+++ Source/UMFPSFTutorial/UMFPSFTutorialGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UMFPSFTutorialGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUMFPSFTutorialGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUMFPSFTutorialGameMode();
+};
+
+
+
